52bc82b6fcd4f893295bdaba54cba5effb872f4f
Karene01 <karenerakotonindrina@gmail.com>
2025-05-04 20:16:41 +0200

    Insertion des données de chaque table

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # ProcesVerbaux-app
+
+## 📋 Liste des tâches
+
+### Initialisation
+- [x] Dupliquer `todo-app`
+- [x] Créer la base
+- [x] Peupler la base
+
+---
+
+### Gestion des copropriétés
+
+---
+
+### Gestion des assemblées générales
+- [ ] Créer une commande `app:debuter-ag`
+- [ ] Créer une commande `app:clore-ag`
+- [ ] Créer une commande `app:generer-pv-ag`
+- [ ] Créer une commande `app:list-ag`
+
+---
+
+### Gestion des copropriétaires
+- [ ] Créer une commande `app:list-coproprietaires`
+
+---
+
+### Gestion des présences
+- [ ] Créer une commande `app:add-presence`
+- [ ] Créer une commande `app:add-representation`
+- [ ] Créer une commande `app:list-presences`
+
+---
+
+### Gestion des questions
+- [ ] Créer une commande `app:add-question`
+- [ ] Créer une commande `app:list-questions`
+- [ ] Créer une commande `app:add-question-vote`
+- [ ] Créer une commande `app:add-question-discussion`
+
+---
+
+### Gestion des votes
+- [ ] Créer une commande `app:calculer-decision-vote`
--- src/Entity/Vote.php
+++ src/Entity/Vote.php
@@ -9,21 +9,19 @@ use Doctrine\ORM\Mapping as ORM;
 class Vote
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(name: "id_vote", type: "integer")]
-    private ?int $id = null;
+    #[ORM\Column(name: "id_vote", type: "string")]
+    private ?string $id = null;
 
-    #[ORM\Column(name: "type_vote", type: "string", length: 10)]
-    private ?string $typeVote = null; // valeurs attendues : 'pour', 'contre', 'abstention'
-
-    #[ORM\Column(name: "poids_vote", type: "float")]
-    private ?float $poids = null;
-
-    #[ORM\ManyToOne(targetEntity: Coproprietaire::class)]
-    #[ORM\JoinColumn(name: "id_coproprietaire", referencedColumnName: "id_coproprietaire")]
-    private ?Coproprietaire $votant = null;
+    #[ORM\Column(name: "valeur_vote", type: "string", length: 20)]
+    private ?string $valeurVote = null; // valeurs attendues : 'Pour', 'Contre', 'Abstention'
 
     #[ORM\ManyToOne(targetEntity: Question::class)]
     #[ORM\JoinColumn(name: "id_question", referencedColumnName: "id_question")]
     private ?Question $question = null;
+
+    #[ORM\ManyToOne(targetEntity: Participation::class)]
+    #[ORM\JoinColumn(name: "id_participation", referencedColumnName: "id_participation")]
+    private ?Participation $participation = null;
+
+    // Ajoute ici des getters/setters si nécessaire
 }
